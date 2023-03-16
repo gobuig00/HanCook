802fa313a4319f7842e1fbd911789fd9f5ad786f
young <gobuig00@naver.com>
2023-03-16 14:49:43 +0900

    [back] feature : 회원가입, 로그아웃, 음식기록등록 추가 (#26)

--- backend/src/main/java/com/wooseung/hancook/service/FoodRecordService.java
+++ backend/src/main/java/com/wooseung/hancook/service/FoodRecordService.java
@@ -1,8 +1,9 @@
 package com.wooseung.hancook.service;
 
 import com.wooseung.hancook.api.request.FoodRecordRequestDto;
+import com.wooseung.hancook.db.entity.User;
 
 public interface FoodRecordService {
-    void insertFoodRecord(FoodRecordRequestDto foodRecordRequestDto);
+    void insertFoodRecord(FoodRecordRequestDto foodRecordRequestDto, String email);
 
 }
