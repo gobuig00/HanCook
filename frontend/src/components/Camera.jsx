4e9b21bd1fcd0ab4264fcb1f7311264fb78eb4fe
njh3321 <njh3321@naver.com>
2023-03-17 10:29:35 +0900

    [front] feature : 이미지 파일 메이커 함수 수정 및 머지 전 준비 (#20)

--- frontend/src/components/Camera.jsx
+++ frontend/src/components/Camera.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function Camera({isVideoStart}) {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
-  const [choice, setChoice] = useState('food')
+  // const [choice, setChoice] = useState('food')
 
   useEffect(() => {
     getVideo();
@@ -26,14 +26,13 @@ export default function Camera({isVideoStart}) {
       console.error(err);
     }
   };
-  const clickRadioBtn = (btnName) => {
-    setChoice(btnName)
-  };
+  // const clickRadioBtn = (btnName) => {
+  //   setChoice(btnName)
+  // };
 
   const getCapture = () => {
     const canvas = canvasRef.current;
     const video = videoRef.current;
-
     if (canvas && video) {
       const context = canvas.getContext('2d');
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -43,10 +42,7 @@ export default function Camera({isVideoStart}) {
     }
   };
   const imageMaker = (dataUrl) => {
-    const blob = new Blob([dataUrl], {type: 'image/png'});
-    const imageUrl = URL.createObjectURL(blob);
     const img = new Image();
-    img.src = imageUrl;
     img.onload = () => {
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
@@ -54,14 +50,16 @@ export default function Camera({isVideoStart}) {
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
       canvas.toBlob(blob => {
+        console.log('111111111111')
         const imageFile = new File([blob], 'image.png', { type: 'image/png' });
-        axiosFunc(imageFile)
+        axiosFunc(imageFile);
       }, 'image/png', 1);
     };
-    
+    img.src = dataUrl;
   };
 
   const axiosFunc = (imageFile) => {
+    
     const apiEndpointUrl = 'https://18c2a61d-b6aa-4581-b1df-02f9c7337ce0.api.kr-central-1.kakaoi.io/ai/vision/4906cb06cc5f42858a61ccbf10bd0f74';
     const apiKey = 'cc044e06145b8c458fba2fb91ed7629f';
 
@@ -92,13 +90,13 @@ export default function Camera({isVideoStart}) {
       <div>
         <video ref={videoRef} />
       </div>
-      <div className='radioBtnGroup'>
+      {/* <div className='radioBtnGroup'>
         <input type="radio" id="food" checked={choice==='food'} onClick={()=> clickRadioBtn('food')}/>
         <label htmlFor="food">food</label>
 
         <input type="radio" id="text" checked={choice==='text'} onClick={()=> clickRadioBtn('text')}/>
         <label htmlFor="text">text</label>
-      </div>
+      </div> */}
       <div>
         <button type="button" onClick={getCapture}> capture </button>
         <canvas ref={canvasRef} style={{display: 'none'}} />
