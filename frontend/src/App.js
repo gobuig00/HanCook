0e33363869ea4bed02d4d3e49a2d58666a4b5163
OctoHun <tjd_gns12@naver.com>
2023-03-17 17:13:36 +0900

    [front] feature : 스타트페이지 구성 수정 (#27)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Start from './Start';
 import Main from './Main';
+// import Login from './components/acoounts/LogIn'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Start />} />
       <Route path='/main' element={<Main />} />
+      {/* <Route path='/login' element={<Login />} /> */}
     </Routes>
   );
 }
