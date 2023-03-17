a53d2ab556a0d6d5f289ac0249c682eb5a828d8a
eotjr990 <eotjr990@naver.com>
2023-03-17 12:07:03 +0900

    [back] feature : 음식인식 API 추가 (#26)

---
+++ backend/src/main/java/com/wooseung/hancook/api/controller/FoodRecognitionController.java
@@ -0,0 +1,32 @@
+package com.wooseung.hancook.api.controller;
+
+import com.wooseung.hancook.service.FoodRecognitionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/food")
+@RequiredArgsConstructor
+public class FoodRecognitionController {
+
+    private final FoodRecognitionService foodRecognitionService;
+
+    @PostMapping("/recognize")
+    public ResponseEntity<String> recognizeFood(@RequestParam("image") MultipartFile image) {
+        try {
+            String result = foodRecognitionService.recognizeFood(image);
+            return ResponseEntity.ok(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
