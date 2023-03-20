14999f6bdcc4af59af9bdbfe4a8392a703617245
OctoHun <tjd_gns12@naver.com>
2023-03-20 11:22:11 +0900

    [front] feature : 스타트페이지 상세 구성 완료 (#27)

--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -7,6 +7,7 @@ import seasonal2 from '../images/seasonal2.jpg';
 import seasonal3 from '../images/seasonal3.jpg';
 import seasonal4 from '../images/seasonal4.jpg';
 import takepicture from '../images/takepicture.jpg';
+import buyimage from '../images/buyimage.jpg';
 
 function Start() {
   return (
@@ -18,20 +19,42 @@ function Start() {
         <div className='main-first'>
           <img className="take-picture-image" src={takepicture} alt="" />
           <div className='main-text-container'>
-            <p className='main-text-lineheight'><span className='green-text'>Searching </span><span className='black-text'>for recipes</span></p>
-            <p className='main-text-lineheight'><span className='black-text'>through </span><span className='orange-text'>pictures</span></p>
+            <p className='main-text-lineheight'><span className='green-text'>Searching&nbsp;</span><span className='black-text'>for recipes</span></p>
+            <p className='main-text-lineheight'><span className='black-text'>through&nbsp;</span><span className='orange-text'>pictures</span></p>
           </div>
           <div className='main-small-text-container'>
-            <p className='main-smaill-text-lineheight'>you can get information about food</p>
+            <p className='main-smaill-text-lineheight'>You can get information about food</p>
             <p className='main-smaill-text-lineheight'>just by taking pictures.</p>
           </div>
-          
         </div>
+        <div className='line main-line'></div>
         <div className='main-second'>
-
+          <div className='seasonal-image-container'>
+            <img className='seasonal-image' src={seasonal1} alt="" />
+            <img className='seasonal-image' src={seasonal2} alt="" />
+            <img className='seasonal-image' src={seasonal3} alt="" />
+            <img className='seasonal-image' src={seasonal4} alt="" />
+          </div>
+          <div className='main-text-container'>
+            <p className='main-text-lineheight'><span className='black-text'>Get&nbsp;</span><span className='green-text'>seasonal&nbsp;</span><span className='black-text'>food</span></p>
+            <p className='main-text-lineheight'><span className='orange-text'>recommendations</span></p>
+          </div>
+          <div className='main-small-text-container'>
+            <p className='main-smaill-text-lineheight'>You can get information about food</p>
+            <p className='main-smaill-text-lineheight'>just by taking pictures.</p>
+          </div>
         </div>
-        <div className='third'>
-
+        <div className='line main-line'></div>
+        <div className='main-third'>
+          <img className='buyimage' src={buyimage} alt="" />
+          <div className='main-text-container'>
+            <p className='main-text-lineheight'><span className='green-text'>Price&nbsp;</span><span className='black-text'>comparison</span></p>
+            <p className='main-text-lineheight'><span className='black-text'>by&nbsp;</span><span className='orange-text'>mart</span></p>
+          </div>
+          <div className='main-small-text-container'>
+            <p className='main-smaill-text-lineheight'>You can get information about food</p>
+            <p className='main-smaill-text-lineheight'>just by taking pictures.</p>
+          </div>
         </div>
       </main>
       <footer>
