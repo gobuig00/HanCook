f3068762db36098eebc4e516c439eaf9cba56101
njh3321 <njh3321@naver.com>
2023-03-23 17:55:34 +0900

    [front] feature : 도넛 컴포넌트 수정 (#46)

--- frontend/src/components/Donut.jsx
+++ frontend/src/components/Donut.jsx
@@ -3,61 +3,67 @@ import { Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend, Title);
 
-export const data = {
-  labels: ['Carbohydrate', 'Protein', 'Fat'],
-  datasets: [
-    {
-      // data에 탄수화물, 단백질, 지방 순서로 변수 할당
-      data: [96, 2, 3],
-      backgroundColor: [
-        'rgba(55, 183, 255, 1)',
-        'rgba(105, 181, 80, 1)',
-        'rgba(218, 162, 79, 1)',
-      ],
-      borderColor: [
-        'rgba(55, 183, 255, 1)',
-        'rgba(105, 181, 80, 1)',
-        'rgba(218, 162, 79, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-export const options = {
-  plugins: {
-    title: {
-      display: true,
-      text: 'Nutrition',
-      align: 'start',
-      font: {
-        size: 30,
-        family: 'bold',
+export default function Donut({ keyList, valueList, title }) {
+  const data = {
+    labels: keyList,
+    datasets: [
+      {
+        data: valueList,
+        backgroundColor: [
+          'rgba(55, 183, 255, 1)',
+          'rgba(105, 181, 80, 1)',
+          'rgba(218, 162, 79, 1)',
+        ],
+        borderColor: [
+          'rgba(55, 183, 255, 1)',
+          'rgba(105, 181, 80, 1)',
+          'rgba(218, 162, 79, 1)',
+        ],
+        borderWidth: 1,
       },
-      color: 'black',
-    },
-    tooltip: {
-      callbacks: {
-        label: function (context) {
-          const value = context.parsed || 0;
-          return `${value}% `;
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: title,
+        align: 'start',
+        font: {
+          size: 24,
+          family: 'bold',
+        },
+        color: '#4d820e',
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const value = context.parsed || 0;
+            return `${value}% `;
+          },
+        },
+      },
+      legend: {
+        position: 'right',
+        labels: {
+          boxWidth: 15,
+          boxHeight: 15,
+          font: {
+            size: 15,
+            family: 'semibold',
+          },
         },
       },
     },
-    legend: {
-      position: 'right',
-      labels: {
-        boxWidth: 15,
-        boxHeight: 15,
-        font: {
-          size: 15,
-          family: 'semibold',
-        }
-      }
-    }
-  },
-};
+  };
 
-export default function Donut() {
-  return <Doughnut data={data} options={options} />;
+  return (
+    <Doughnut
+      data={data}
+      options={options}
+    />
+  );
 }
+
