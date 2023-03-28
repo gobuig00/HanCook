a520feb0ce4548a16c1b33fcedb334f8fc97f034
njh3321 <njh3321@naver.com>
2023-03-28 15:30:20 +0900

    [front] feature : cart receipt 카카오톡 공유하기 추가 중 (#58)

---
+++ frontend/src/components/Cart/CartUtils.js
@@ -0,0 +1,38 @@
+export const toggleExpandUtil = (prevExpanded, ingredient) => ({
+    ...prevExpanded,
+    [ingredient]: !prevExpanded[ingredient],
+  });
+  
+  export const deleteIngredientUtil = (prevCartItems, ingredient) => {
+    const newCartItems = { ...prevCartItems };
+    delete newCartItems[ingredient];
+    return newCartItems;
+  };
+  
+  export const selectItemUtil = (prevSelectedItems, item, index) => {
+    if (prevSelectedItems[index].some((selectedItem) => selectedItem.title === item.title)) {
+      console.log('Item already added.');
+      return prevSelectedItems;
+    }
+    const newSelectedItems = [...prevSelectedItems];
+    newSelectedItems[index].push(item);
+    return newSelectedItems;
+  };
+  
+  export const deleteSelectedItemUtil = (prevSelectedItems, martIndex, itemIndex) => {
+    const newSelectedItems = [...prevSelectedItems];
+    newSelectedItems[martIndex] = newSelectedItems[martIndex].filter((item, index) => index !== itemIndex);
+    return newSelectedItems;
+  };
+  
+  export const getTotalPriceByMartUtil = (items) => {
+    return items.reduce((totalPrice, currentItem) => {
+      return totalPrice + parseInt(currentItem.price);
+    }, 0);
+  };
+  
+  export const getTotalPriceUtil = (selectedItems, getTotalPriceByMart) => {
+    return selectedItems.reduce((totalPrice, currentMartItems) => {
+      return totalPrice + getTotalPriceByMart(currentMartItems);
+    }, 0);
+  };
