f4b6b747ea41b22af964d4c949ecf67bae06dc61
OctoHun <tjd_gns12@naver.com>
2023-03-29 12:13:28 +0900

    [front] feature : 카메라 페이지 메뉴 구현 완료 (#63)

---
+++ frontend/src/components/Camera/Menu.jsx
@@ -0,0 +1,53 @@
+import './Menu.css';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useState } from 'react';
+
+const styles = {
+  slide: {
+    minHeight: 70,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'semibold',
+  },
+  slide1: {
+  },
+  slide2: {
+  },
+};
+
+const Menu = () => {
+  const [selectedSlide, setSelectedSlide] = useState(0);
+
+  const handleClick = (index) => {
+    setSelectedSlide(index);
+  };
+
+  return (
+    <div className='menu-carousel-position'>
+      <div className='selected-background'></div> 
+      <Carousel
+        showStatus={false}
+        showThumbs={false}
+        showIndicators={false}
+        showArrows={false}
+        swipeable={true}
+        dynamicHeight={false}
+        emulateTouch={false}
+        selectedItem={selectedSlide}
+        onChange={(index) => setSelectedSlide(index)}
+      >
+        <div style={Object.assign({}, styles.slide, styles.slide1)} onClick={() => handleClick(0)}>
+          <span className='carousel-selected-text'>Food</span> 
+        </div>
+        <div style={Object.assign({}, styles.slide, styles.slide2)} onClick={() => handleClick(1)}>
+          <span className='carousel-selected-text'>Text</span>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Menu;
