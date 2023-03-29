12a15fb1558a8c9f327fd9cc4ab4c282d8a3f6a2
eotjr990 <eotjr990@naver.com>
2023-03-29 17:23:19 +0900

    [back] feature : 음식 영양소 API 추가 (#69)

---
+++ backend/src/main/java/com/wooseung/hancook/db/entity/FoodNutrient.java
@@ -0,0 +1,36 @@
+package com.wooseung.hancook.db.entity;
+
+import com.wooseung.hancook.api.response.FoodNutrientResponseDto;
+import com.wooseung.hancook.utils.ModelMapperUtils;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class FoodNutrient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no")
+    private Long no;
+    private String name;
+    @Column(name = "serving_size")
+    private int servingSize;
+
+    private String unit;
+    private double kcal;
+    private double carb;
+    private double protein;
+    private double fat;
+    private double sugar;
+    private double cholesterol;
+
+    public static FoodNutrient of(FoodNutrientResponseDto foodNutrientResponseDto){
+        FoodNutrient foodNutrientEntity = ModelMapperUtils.getModelMapper().map(foodNutrientResponseDto, FoodNutrient.class);
+        return foodNutrientEntity;
+    }
+}
