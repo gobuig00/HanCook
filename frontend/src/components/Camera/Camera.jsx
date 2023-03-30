0cf56e2b3834acd1c19d09a96d878f10d4004ea8
오성훈 <tjd_gns12@naver.com>
2023-03-30 17:26:30 +0900

    extra : 카메라 전면으로 수정

--- frontend/src/components/Camera/Camera.jsx
+++ frontend/src/components/Camera/Camera.jsx
@@ -46,7 +46,7 @@ export default function Camera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: {
-            facingMode: 'environment',
+            facingMode: { exact: 'environment' },
           },
         });
         const video = videoRef.current;
