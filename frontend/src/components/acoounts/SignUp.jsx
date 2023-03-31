48d3e1bb60456c6b93ad461b0230a1a8c8f5a134
OctoHun <tjd_gns12@naver.com>
2023-03-31 16:14:10 +0900

    [front] feature : 장바구니, 오늘 먹은 목록 추가 기능 (#79)

--- frontend/src/components/acoounts/SignUp.jsx
+++ frontend/src/components/acoounts/SignUp.jsx
@@ -58,7 +58,6 @@ export default function SignUp() {
             email,
 
         }
-
         try {
             const response = await axios.post(`${process.env.REACT_APP_API_URL}/user/join`, {
                 email,
