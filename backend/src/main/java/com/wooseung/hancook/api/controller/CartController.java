76563aeb2aeec23b2f8d76d14889da8e2e4dbc42
eotjr990 <eotjr990@naver.com>
2023-03-31 14:15:47 +0900

    [back] feature : 장바구니 API 생성 (#86)

---
+++ backend/src/main/java/com/wooseung/hancook/api/controller/CartController.java
@@ -0,0 +1,44 @@
+package com.wooseung.hancook.api.controller;
+
+import com.wooseung.hancook.api.response.BaseResponseBody;
+import com.wooseung.hancook.api.response.ComponentResponseDto;
+import com.wooseung.hancook.api.service.CartService;
+import com.wooseung.hancook.api.service.RecipeService;
+import com.wooseung.hancook.common.auth.UserDetails;
+import com.wooseung.hancook.db.entity.Component;
+import com.wooseung.hancook.db.entity.Ingredient;
+import com.wooseung.hancook.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<BaseResponseBody> addToCartByRecipeId(@RequestParam("recipeId") Long recipeId, @AuthenticationPrincipal UserDetails userDetails) {
+        cartService.addIngredientToCartByRecipeId(recipeId, userDetails.getEmail());
+        return new ResponseEntity<>(new BaseResponseBody("SUCCESS", 201), HttpStatus.CREATED);
+    }
+
+//    @PostMapping("/add")
+//    public ResponseEntity<BaseResponseBody> addToCartByComponentId(@RequestParam("componentId") Long componentId, @AuthenticationPrincipal UserDetails userDetails) {
+//        cartService.addIngredientToCartByRecipeId(recipeId, userDetails.getEmail());
+//        return new ResponseEntity<>(new BaseResponseBody("SUCCESS", 201), HttpStatus.CREATED);
+//    }
+
+//    @GetMapping("/get")
+//    public ResponseEntity<List<FoodRecordResponseDto>> getFoodRecordByUser(@AuthenticationPrincipal UserDetails userDetails) {
+//        Optional<User> user = userRepository.findByEmail(userDetails.getEmail());
+//        return ResponseEntity.status(HttpStatus.OK).body(foodRecordService.getFoodRecordById(user.get().getId()));
+//    }
+}
---
+++ backend/src/main/java/com/wooseung/hancook/api/service/CartServiceImpl.java
@@ -0,0 +1,42 @@
+package com.wooseung.hancook.api.service;
+
+import com.wooseung.hancook.api.response.ComponentResponseDto;
+import com.wooseung.hancook.common.exception.ApiException;
+import com.wooseung.hancook.common.exception.ExceptionEnum;
+import com.wooseung.hancook.db.entity.Cart;
+import com.wooseung.hancook.db.entity.User;
+import com.wooseung.hancook.db.repository.CartRepository;
+import com.wooseung.hancook.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service("cartService")
+@RequiredArgsConstructor
+@Slf4j
+public class CartServiceImpl implements CartService {
+
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+
+    private final RecipeService recipeService;
+
+    @Override
+    @Transactional
+    public void addIngredientToCartByRecipeId(Long recipeId, String email) {
+        // 레시피 아이디로 재료 리스트 받기
+        List<ComponentResponseDto> componentResponseDtoList = recipeService.getIngredientByRecipeId(recipeId, 0);
+
+        // 이메일에 맞는 유저 검색
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ApiException(ExceptionEnum.MEMBER_NOT_EXIST_EXCEPTION));
+
+        for (ComponentResponseDto componentResponseDto : componentResponseDtoList) {
+            String componentName = componentResponseDto.getName();
+            cartRepository.save(new Cart(user, componentName));
+        }
+    }
+}
