94d16bfd20a08d8f64eb56534c32aa747c6f4e89
eotjr990 <eotjr990@naver.com>
2023-04-03 22:07:27 +0900

    [back] feature : getChange 수정 (#93)

--- backend/src/main/java/com/wooseung/hancook/db/repository/DealRepository.java
+++ backend/src/main/java/com/wooseung/hancook/db/repository/DealRepository.java
@@ -29,9 +29,8 @@ public interface DealRepository extends JpaRepository<Deal, Long> {
     @Query(value = "SELECT t1.medium, t1.small, t1.origin, MIN(t1.price) as min_price, MAX(t2.price) as max_price, ((MAX(t2.price) - MIN(t1.price)) / MIN(t1.price) * 100) as price_difference_percentage FROM (SELECT * FROM deal WHERE deal_date = :sevenDaysAgo) AS t1 JOIN (SELECT * FROM deal WHERE deal_date = :today) AS t2 ON t1.large = t2.large AND t1.medium = t2.medium AND t1.small = t2.small AND t1.origin = t2.origin GROUP BY t1.large, t1.medium, t1.small, t1.origin ORDER BY price_difference_percentage DESC LIMIT 3", nativeQuery = true)
     List<Object[]> findMax(String today, String sevenDaysAgo);
 
-    @Query(value = "SELECT d FROM Deal d WHERE d.medium = :medium AND d.small = :small AND d.origin = :origin AND d.dealDate BETWEEN :startDate AND :endDate order by ASC", nativeQuery = true)
+    @Query(value = "SELECT * FROM deal d WHERE d.medium = :medium AND d.small = :small AND d.origin = :origin AND d.deal_date BETWEEN :startDate AND :endDate order by d.deal_date ASC", nativeQuery = true)
     List<Deal> findDealsByMediumAndSmallAndOriginAndDateRange(String medium, String small, String origin, String startDate, String endDate);
-
 
     @Query(value = "SELECT t1.medium, t1.small, t1.origin, MIN(t1.price) as min_price, MAX(t2.price) as max_price, ((MAX(t2.price) - MIN(t1.price)) / MIN(t1.price) * 100) as price_difference_percentage FROM (SELECT * FROM deal WHERE deal_date = :sevenDaysAgo) AS t1 JOIN (SELECT * FROM deal WHERE deal_date = :today) AS t2 ON t1.large = t2.large AND t1.medium = t2.medium AND t1.small = t2.small AND t1.origin = t2.origin GROUP BY t1.large, t1.medium, t1.small, t1.origin ORDER BY price_difference_percentage ASC LIMIT 3", nativeQuery = true)
     List<Object[]> findMin(String today, String sevenDaysAgo);
