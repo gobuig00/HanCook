4c07a5894e0f34724ffe73665e7a125e438c34c6
eotjr990 <eotjr990@naver.com>
2023-04-05 16:50:19 +0900

    [back] feature : findNineByIngredientId 수정, 예외처리 추가 (#104)

--- backend/src/main/java/com/wooseung/hancook/db/repository/MartRepository.java
+++ backend/src/main/java/com/wooseung/hancook/db/repository/MartRepository.java
@@ -1,8 +1,8 @@
 package com.wooseung.hancook.db.repository;
 
-import com.wooseung.hancook.db.entity.Ingredient;
 import com.wooseung.hancook.db.entity.Mart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +11,7 @@ import java.util.List;
 public interface MartRepository extends JpaRepository<Mart, Long> {
 
 //    List<Mart> findAllByIngredientAndMartAndItemNameIsNotNull(Ingredient ingredient, int mart);
-    List<Mart> findAllByIngredient(Ingredient ingredient);
+
+    @Query(value = "SELECT * FROM mart WHERE ingredient_id = :ingredientId limit 9", nativeQuery = true)
+    List<Mart> findNineByIngredientId(Long ingredientId);
 }
