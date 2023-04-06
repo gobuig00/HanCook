5ce7f1666e3778c97bccee8bce3d2ba3734b2c59
eotjr990 <eotjr990@naver.com>
2023-04-06 21:38:30 +0900

    [back] feature : getRecipeByIngredientList 추가 (#105)

--- backend/src/main/java/com/wooseung/hancook/api/service/RecipeServiceImpl.java
+++ backend/src/main/java/com/wooseung/hancook/api/service/RecipeServiceImpl.java
@@ -194,6 +194,73 @@ public class RecipeServiceImpl implements RecipeService {
         return recipeResponseDtoList;
     }
 
+    @Override
+    public List<RecipeResponseDto> getRecipeByIngredientList(List<String> ingredient, int lan) {
+        // 전체 레시피 Entity List
+        List<Recipe> recipeEntityList = recipeRepository.findAll();
+        // 반환할 레시피 Dto List
+        List<RecipeResponseDto> recipeResponseDtoList = new ArrayList<>();
+        // 레시피에 포함된 전체 재료 정보 Entity List
+        List<Component> componentList = componentRepository.findAll();
+        // 정제된 전체 재료 정보 EntityList
+        List<Ingredient> ingredientList = ingredientRepository.findAll();
+
+        // ingredientList를 돌면서 ref 값이 존재하는 데이터를 찾는다.
+        for (Ingredient ingredientEntity : ingredientList) {
+            if (ingredientEntity.getRef() != null && ingredientEntity.getRef() > 0 && ingredientEntity.getRef() <= 728) {
+                // ref 값이 존재하면 componentList를 돌면서 해당 재료의 이름을 ref 값에 해당하는 이름으로 치환한다.
+                for (Component component : componentList) {
+                    if (component.getName().equals(ingredientEntity.getName())) {
+                        component.setName(ingredientList.get((int) (ingredientEntity.getRef() - 1)).getName());
+                    }
+                }
+            }
+        }
+
+        /*
+         * 전체 레시피 Entity List를 돌면서 해당 레시피 ID를 가진 재료 Entity만을 DB에서 추출할 수 있지만,
+         * 500개의 레시피 Entity List를 돌면서 매번 select문을 수행하니 동작이 느려서 미리 전체 재료 Entity List를 추출하고
+         * 반복문을 통해 검사하는 방법을 사용
+         */
+        // 전체 레시피 Entity List를 돌면서 선택된 재료들(ingredient)가 모두 포함된 레시피를 찾아 recipeDtoList에 추가
+        for (Recipe recipeEntity : recipeEntityList) {
+            int count = 0;
+            HashSet<String> ingredientSet = new HashSet<>();
+            for (String ingredientStr : ingredient) {
+                int flag = detectLanguageService.detectLanguage(ingredientStr);
+
+                // 입력받은 재료가 영어라면 한글로 변환
+                if (flag == 1) ingredientStr = papagoTranslationService.translateEnglishIntoKorean(ingredientStr);
+
+                ingredientSet.add(ingredientStr);
+            }
+            // 전체 재료 정보 Entity List를 돌면서 레시피 ID가 같고, 선택된 재료들 중 현재 재료 Entity의 이름이 포함되어있는 경우 count++
+            for (Component component : componentList) {
+                if (component.getRecipeId() == recipeEntity.getRecipeId() && ingredientSet.contains(component.getName())) {
+                    ingredientSet.remove(component.getName());
+                    count++;
+                }
+                // count 값이 ingredient의 크기와 같아지면 선택된 재료들이 모두 포함되었음을 의미
+                if (count == ingredient.size()) {
+                    RecipeResponseDto recipeResponseDto = RecipeResponseDto.of(recipeEntity);
+
+                    // 영문일때
+                    if (lan == 1) {
+                        // DB에 한글로 저장되어 있어 영어로 번역해서 response
+                        recipeResponseDto.setName(papagoTranslationService.translateKoreanIntoEnglish(recipeEntity.getName()));
+                        recipeResponseDto.setDescription(papagoTranslationService.translateKoreanIntoEnglish(recipeEntity.getDescription()));
+                    }
+
+                    // 반환할 DtoList에 현재 RecipeEntity를 Dto로 변환하여 추가
+                    recipeResponseDtoList.add(recipeResponseDto);
+                    break;
+                }
+            }
+        }
+
+        return recipeResponseDtoList;
+    }
+
     // lan : (한글 : 0) / (영문 : 1)
     @Override
     public List<ComponentResponseDto> getIngredientByRecipeId(Long recipeId, int lan) {
