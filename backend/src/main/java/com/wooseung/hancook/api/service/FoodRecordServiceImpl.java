84972354f5a75e7ecac6d05e6dc0209b3b41fd91
이우승 <ss6116@daum.net>
2023-04-07 01:48:20 +0900

    hotfix : mart trouble shooting (#110)

--- backend/src/main/java/com/wooseung/hancook/api/service/FoodRecordServiceImpl.java
+++ backend/src/main/java/com/wooseung/hancook/api/service/FoodRecordServiceImpl.java
@@ -59,7 +59,7 @@ public class FoodRecordServiceImpl implements FoodRecordService {
     public List<FoodRecordResponseDto> getFoodRecordById(Long id) {
         // ID로 찾은 음식 기록 Entity List
         List<FoodRecord> foodRecordEntityList = foodRecordRepository.findAllByUserId(id);
-
+        System.out.println(foodRecordEntityList.size());
         List<FoodRecordResponseDto> foodRecordResponseDtoList = new ArrayList<>();
 
         for (FoodRecord foodRecordEntity : foodRecordEntityList) {
